dd8055c31251ad6ad60654b13e5d170f540fd859
Juan Oscco <juan.osccomori@gmail.com>
2024-03-17 16:08:45 -0500

    feat: adding .env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 import app from "./app";
 import { AppDataSource } from "./db";
+
 async function main() {
   try {
     await AppDataSource.initialize();
     app.listen(8080);
-    console.log(`Server on port http://localhost:${8080}`);
+    console.log(`Server on port http://localhost:${8080}/api-docs`);
   } catch (error) {
     console.error(error);
   }
