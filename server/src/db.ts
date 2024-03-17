e48124bc1a4a2d83ade4b162f71a5d861bf56977
Juan Oscco <juan.osccomori@gmail.com>
2024-03-17 16:31:55 -0500

    feat: implement env in db and index

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,14 +1,17 @@
+import dotenv from "dotenv";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 
+dotenv.config();
+
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "toor",
-    database: "betmaster_db",
-    synchronize: true,
-    // logging: true,
-    entities: [User],
-  });
+  type: "postgres",
+  host: process.env.HOSTNAME,
+  port: 5432,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  // logging: true,
+  entities: [User],
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
+import dotenv from "dotenv";
 import app from "./app";
 import { AppDataSource } from "./db";
+
+dotenv.config();
 
 async function main() {
   try {
     await AppDataSource.initialize();
-    app.listen(8080);
-    console.log(`Server on port http://localhost:${8080}/api-docs`);
+    app.listen(process.env.PORT || 8080);
+    console.log(
+      `Server on port http://localhost:${process.env.PORT || 8080}/api-docs`
+    );
   } catch (error) {
     console.error(error);
   }
